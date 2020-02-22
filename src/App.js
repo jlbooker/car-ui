87b691fd2167fc58ae7c20e7d49a9f6e781128fc
Jeremy Booker <jeremy.booker@gmail.com>
2020-02-22 17:23:19 -0500

    Cleanup default page and images. Add cars.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import cars from "./cars.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>SAS Web UI Developer Project</h1>
+        <p>See the Instructions.md files for what to do.</p>
+        There are {cars.length} cars in the data.
+      </div>
   );
 }
 
