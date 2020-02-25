10bfdcbaf0668e9ae97100e93a1d2f23bdd7d257
Jeremy Booker <jeremy.booker@gmail.com>
2020-02-24 22:14:55 -0500

    Add up/down arrow keyboard shortcut handling.

--- src/Car.js
+++ src/Car.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+function handleKeyPress(e){
+    e.preventDefault();
+    if(e.key === 'ArrowDown'){
+        const nextItem = document.activeElement.nextSibling;
+        if(nextItem !== null){
+            nextItem.focus();
+        }
+    }
+
+    if(e.key === 'ArrowUp'){
+        const prevItem = document.activeElement.previousSibling;
+        if(prevItem !== null){
+            prevItem.focus()
+        }
+
+    }
+}
+
 function Car(props) {
 
     return (
-        <li>
+        <li tabIndex="0" onKeyDown={handleKeyPress}>
             {props.make} {props.model}, {props.type} from {props.origin}
         </li>
     )
