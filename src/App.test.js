08ea7910c6a86798d48f2822d9e53cdd23fbe96a
Jeremy Booker <jeremy.booker@gmail.com>
2020-02-26 10:49:18 -0500

    Add a few more tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,30 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+// In a longer-term project, I'd factor these out into their own files
+import cars from "./cars.json";
+import Car from './Car.js';
+
 // Basic rendering test
 test('Renders without crashing', () => {
-  const app = render(<App />);
+    const app = render(<App />);
 });
 
-// Displays car count correctly
+// Data set exists
+test('Data set includes 425 cars', () => {
+    expect(cars.length).toBe(425);
+});
 
-// Car Renders car description correctly
+test('Displays dataset size correctly', () => {
+    const { getByText } = render(<App />);
+    const uiElement = getByText(/There are 425 cars in the data/i);
+    expect(uiElement).toBeInTheDocument();
+});
 
-// Filter text filters list
+test('Car component renders description text', () => {
+    const { getByText } = render(<Car make="Tesla" model="Model 3" type="sedan" origin="California" />);
+    const uiElement = getByText(/Tesla Model 3, sedan from California/i);
+    expect(uiElement).toBeInTheDocument();
+});
+
+// TODO CarList renders list, text in filter box filters results, simulate click on clear button, etc
